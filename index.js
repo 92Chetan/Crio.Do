ecdf24fb7c614e3af87c797fa476146d2846f7da
92Chetan <chetansinghal@outlook.com>
2023-09-13 05:11:30 +0000

    Write & Push a Programs to display the numbers from 1 to 15 using for , while & do while loop.

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
-08/09/2023
-// First Program - Hello World !!
-console.log('Hello World !!');
-// console.log ('This is the test push to git !!');
-// console.log ('This is the Second test push to git !!');
-// console.log ('This is the Third test push to git !!');
-console.log ('This is the Fourth Test Push to the Git Hub')
+// 08 / 09 / 2023
+// // First Program - Hello World !!
+// console.log('Hello World !!');
+// // console.log ('This is the test push to git !!');
+// // console.log ('This is the Second test push to git !!');
+// // console.log ('This is the Third test push to git !!');
+// console.log('This is the Fourth Test Push to the Git Hub')
 
-10/09/2023
-// Creating a Simple Calculator which takes two input, performs the operations on input and shows the output.
+// 10 / 09 / 2023
+// // Creating a Simple Calculator which takes two input, performs the operations on input and shows the output.
 
-//Select the operation that needs to perform.
-const operator = prompt('Enter the operator (either +, -, * or / ): ');
+// //Select the operation that needs to perform.
+// const operator = prompt('Enter the operator (either +, -, * or / ): ');
 
-//Take the First and second Number as an input.
-const Num1 = parseFloat (prompt('Enter the First Number : '));
-const Num2 = parseFloat (prompt('Enter the Second Number : '));
+// //Take the First and second Number as an input.
+// const Num1 = parseFloat(prompt('Enter the First Number : '));
+// const Num2 = parseFloat(prompt('Enter the Second Number : '));
 
-let result;
+// let result;
 
-//Using if...elseif...else stmt.
-if (operator == '+')
-{
-  result = Num1 + Num2;
-}
-else if (operator == '-')
-{
-  result = Num1 - Num2;
-}
-else if (operator == '*')
-{
-  result = Num1 * Num2;
-}
-else {
-  result = Num1 / Num2;
-}
+// //Using if...elseif...else stmt.
+// if (operator == '+') {
+//   result = Num1 + Num2;
+// }
+// else if (operator == '-') {
+//   result = Num1 - Num2;
+// }
+// else if (operator == '*') {
+//   result = Num1 * Num2;
+// }
+// else {
+//   result = Num1 / Num2;
+// }
 
 // Display the result 
 
-console.log(`${Num1} ${operator} ${Num2} = ${result}`);
+// console.log(`${Num1} ${operator} ${Num2} = ${result}`);
 
 
-// 11/9/2023
+//https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+
+
+// 11 / 9 / 2023
 // Write a simple Js code to dispaly date and time in console.
 
-let datetime = new Date().toLocaleString();
-console.log (datetime);
+// let datetime = new Date().toLocaleString();
+// console.log(datetime);
 
+// 13/9/2023
+
+// Write a program to print the numbers from 1 to 15 using for loop.
+
+let i =1;
+for (i=1; i<=15; i++){
+  console.log(i);
+}
+
+//Write a program to print the numbers from 1 to 15 using a while loop.
+
+i =1;
+while(i<=15)
+  {
+    console.log(i);
+    i++;
+  }
+
+//Write a program to print the number from 1 to 25 using do while loop.
+
+i=1;
+do{
+  console.log(i);
+  i++;
+}while(i<=25);
