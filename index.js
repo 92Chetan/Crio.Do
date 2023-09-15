b6b62a5dd9f31c76a48d709feff9ef9c6f0a0b9e
92Chetan <chetansinghal@outlook.com>
2023-09-15 14:31:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Problem 1 
+// Write a for loop condition to print the numbers from 1 to 10.
+
+for (let i=1; i<=10; i++){
+ console.log (i); 
+}
+
+// Problem 2 
+// Write a program to print the even numbers b/w 1 to 50 using for loop.
+
+for (let i=1; i<=50; i++){
+if(i%2==0){
+  console.log(i);
+}
+
+}
+
+// Problem 3 
+// Write a program to print the odd numbers b/w 1 to 50 using for loop.
+
+for (let i=1; i<=50; i++){
+  if(i%2 !=0){
+    console.log(i);
+  }
+}
