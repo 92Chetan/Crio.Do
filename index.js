ffcabb407879d9f889db00d34889ead6033f218c
92Chetan <chetansinghal@outlook.com>
2023-09-16 18:05:32 +0000

    Switch Statement to print month based on the user input.

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-// // Problem 1 
-// // Write a for loop condition to print the numbers from 1 to 10.
+// Problem 1 
+// Write a for loop condition to print the numbers from 1 to 10.
 
-// for (let i=1; i<=10; i++){
-//  console.log (i); 
-// }
+for (let i=1; i<=10; i++){
+ console.log (i); 
+}
 
-// // Problem 2 
-// // Write a program to print the even numbers b/w 1 to 50 using for loop.
+// Problem 2 
+// Write a program to print the even numbers b/w 1 to 50 using for loop.
 
-// for (let i=1; i<=50; i++){
-// if(i%2==0){
-//   console.log(i);
-// }
+for (let i=1; i<=50; i++){
+if(i%2==0){
+  console.log(i);
+}
 
-// }
+}
 
-// // Problem 3 
-// // Write a program to print the odd numbers b/w 1 to 50 using for loop.
+// Problem 3 
+// Write a program to print the odd numbers b/w 1 to 50 using for loop.
 
-// for (let i=1; i<=50; i++){
-//   if(i%2 !=0){
-//     console.log(i);
-//   }
-// }
+for (let i=1; i<=50; i++){
+  if(i%2 !=0){
+    console.log(i);
+  }
+}
 
-// // Problem 4
-// // Write a program to print the numbers from 1 to 15 using while loop.
+// Problem 4
+// Write a program to print the numbers from 1 to 15 using while loop.
 
-//  let n = 1;
-// while(n<=15){
-//   console.log(n++);
+ let n = 1;
+while(n<=15){
+  console.log(n++);
 
-// }
+}
 
 
-// // Problem 5 
-// // Write a program to print the number from 1 to 15 using do-while loop.
+// Problem 5 
+// Write a program to print the number from 1 to 15 using do-while loop.
 
-// let i =1;
-// do{
-//   console.log(i++);
-// }while(i<=15);
+let i =1;
+do{
+  console.log(i++);
+}while(i<=15);
 
 
 // Program 6
