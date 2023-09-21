526b8a815b48ad2705288a5139d54c63bc068bee
92Chetan <chetansinghal@outlook.com>
2023-09-21 11:45:54 +0000

    Commit - To calculate the area & perimeter of a rectangle using functions.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Problem 1 
 // Write a for loop condition to print the numbers from 1 to 10.
 
-for (let i=1; i<=10; i++){
- console.log (i); 
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
 
 // Problem 2 
 // Write a program to print the even numbers b/w 1 to 50 using for loop.
 
-for (let i=1; i<=50; i++){
-if(i%2==0){
-  console.log(i);
-}
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
 
 }
 
 // Problem 3 
 // Write a program to print the odd numbers b/w 1 to 50 using for loop.
 
-for (let i=1; i<=50; i++){
-  if(i%2 !=0){
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 != 0) {
     console.log(i);
   }
 }
@@ -27,8 +27,8 @@ for (let i=1; i<=50; i++){
 // Problem 4
 // Write a program to print the numbers from 1 to 15 using while loop.
 
- let n = 1;
-while(n<=15){
+let n = 1;
+while (n <= 15) {
   console.log(n++);
 
 }
@@ -37,10 +37,10 @@ while(n<=15){
 // Problem 5 
 // Write a program to print the number from 1 to 15 using do-while loop.
 
-let i =1;
-do{
+let i = 1;
+do {
   console.log(i++);
-}while(i<=15);
+} while (i <= 15);
 
 
 // Program 6
@@ -48,7 +48,7 @@ do{
 
 let x = parseInt(prompt("Enter a Number B/w 1 and 12"));
 
-switch (x){
+switch (x) {
   case 1:
     console.log("January");
     break;
@@ -85,56 +85,54 @@ switch (x){
   case 12:
     console.log("December");
     break;
-  Default:
-  console.log("Invalid Input !!")
+    Default:
+    console.log("Invalid Input !!")
     break;
 }
 
 // Problem 7
 // Write a program to print numbers from 1 to 10 but not 3 using if condition.
 
-for (i=1; i<=10; i++)
- {
-   if(i==3)
-   {
-     continue;
-   }
+for (i = 1; i <= 10; i++) {
+  if (i == 3) {
+    continue;
+  }
   console.log(i);
- }
+}
 
 // Program 8
 // Write a program to take 3 numbers as an input from the user and find the greatest among them.
 
 
-let x = parseInt(prompt ("Enter First Number: "));
-let y = parseInt(prompt ("Enter Second Number: "));
-let z = parseInt(prompt ("Enter Third Number: "));
-if (x>y) {
-    if (x>z) {
-      console.log(x +" "+ "Is the Greatest Among Three Numbers")
-    } else {
-      console.log(z +" "+ "Is the Greatest Among Three Numbers")
-    }
-  } else if (y>z) {
-    console.log(y +" "+ "Is the Greatest Among Three Numbers")
+let x = parseInt(prompt("Enter First Number: "));
+let y = parseInt(prompt("Enter Second Number: "));
+let z = parseInt(prompt("Enter Third Number: "));
+if (x > y) {
+  if (x > z) {
+    console.log(x + " " + "Is the Greatest Among Three Numbers")
   } else {
-    console.log(z +" "+ "Is the Greatest Among Three Numbers")
+    console.log(z + " " + "Is the Greatest Among Three Numbers")
   }
+} else if (y > z) {
+  console.log(y + " " + "Is the Greatest Among Three Numbers")
+} else {
+  console.log(z + " " + "Is the Greatest Among Three Numbers")
+}
 
 // Program 9
 // Write a program to print the odd numbers from 7 to 31 using for loop.
 
-for(i=7; i<=31; i++){
-  if(i%2 !=0){
-  console.log(i);
+for (i = 7; i <= 31; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
   }
 }
 
 // Program 10
 // Write a program to print the even numbers for 10 to -20 using for loop.
 
-for (i=-20; i<=10; i++){
-  if(i%2 ==0){
+for (i = -20; i <= 10; i++) {
+  if (i % 2 == 0) {
     console.log(i);
   }
 }
@@ -142,22 +140,22 @@ for (i=-20; i<=10; i++){
 // Program 11
 // Write a program to implement parameterized function:
 
-function sum(num1, num2){
-var sum = num1+num2;
-console.log(sum);
+function sum(num1, num2) {
+  var sum = num1 + num2;
+  console.log(sum);
 }
-  sum(1,2);
+sum(1, 2);
 
 
 //Program 12
 // Write a program to implement sum of two numbers taken as an input from the user using functions.
 
-let a = parseInt(prompt ("Enter the First Number: "));
-let b = parseInt(prompt ("Enter the Second Number: "));
-console.log(sum(a,b));
-function sum(a,b){
-let result = a+b;
-  return(result);
+let a = parseInt(prompt("Enter the First Number: "));
+let b = parseInt(prompt("Enter the Second Number: "));
+console.log(sum(a, b));
+function sum(a, b) {
+  let result = a + b;
+  return (result);
 }
 
 // Program 13
@@ -166,16 +164,16 @@ let result = a+b;
 let x = parseInt(prompt("Enter the First Number: "));
 let y = parseInt(prompt("Enter the Second Number: "));
 let z = parseInt(prompt("Enter the Third Number: "));
-console.log(smallest(x,y,z));
-function smallest(x,y,z){
-  if(x<y && x<z){
-    console.log(x + " "+ "is the smallest.");
-    }
-  else if (y<z && y<x){
-    console.log(y +" "+ "is the smallest.");
+console.log(smallest(x, y, z));
+function smallest(x, y, z) {
+  if (x < y && x < z) {
+    console.log(x + " " + "is the smallest.");
   }
-  else{
-    console.log(z +" "+ "is the smallest.");
+  else if (y < z && y < x) {
+    console.log(y + " " + "is the smallest.");
+  }
+  else {
+    console.log(z + " " + "is the smallest.");
   }
 }
 
@@ -185,8 +183,26 @@ function smallest(x,y,z){
 
 let x = parseInt(prompt("Enter the Dividend"));
 let y = parseInt(prompt("Enter the Divisor"));
-console.log(find_Remainder(x,y));
-function find_Remainder (x,y){
-  remainder = x%y;
+console.log(find_Remainder(x, y));
+function find_Remainder(x, y) {
+  remainder = x % y;
   return (remainder);
+}
+
+//Program 15
+// Write a program to find the area and perimeter of a rectangle using functions (take the input from the user).
+
+let x = parseInt(prompt("Enter the Lenght of the rectangle : "));
+let y = parseInt(prompt("Enter the width of the rectangle :"));
+console.log(calculate_Area(x,y));
+console.log(Perimeter(x,y));
+
+function calculate_Area(x,y){
+  Area = x * y;
+  return(Area);
+}
+
+function Perimeter(x,y) {
+  Perimeter = 2*(x+y);
+  return(Perimeter);
 }
